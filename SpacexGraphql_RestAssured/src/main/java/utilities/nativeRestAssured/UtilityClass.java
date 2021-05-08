0fd35207e53f71e603db9293079f7506c3d4efe5
HiTech <mhammad101088@gmail.com>
2021-05-08 22:44:08 +0200

    remove code duplication

--- SpacexGraphql_RestAssured/src/main/java/utilities/nativeRestAssured/UtilityClass.java
+++ SpacexGraphql_RestAssured/src/main/java/utilities/nativeRestAssured/UtilityClass.java
@@ -11,103 +11,59 @@ public final class UtilityClass {
 	// ******* variables *******//
 	private static org.json.JSONObject jsonObject = new org.json.JSONObject();
 	private static RequestSpecification request = RestAssured.given();
-	public static final String BASE_URI = "https://api.spacex.land/"; //should be changed for every project
-	private static final String END_POINT = "graphql/"; // common end point for every graphql request
-
-	// ******* constructor *******//
-	private UtilityClass() {
-		jsonObject = new org.json.JSONObject();
-	}
+	public static final String BASE_URI = "https://api.spacex.land/graphql/";    //should be changed for every project
 
 	// ******* request related methods *******//
-	
-	/**
-	 * MANDATORY <br>
-	 * set the request query or mutation, the request cannot be sent without a query or mutation.
-	 * @param query
-	 */
 	private static void setQuery(String query) {
 		jsonObject.put("query", query);
 	}
 
-	/**
-	 * OPTIONAL<br>
-	 * set the request variables, the request can be sent without variables
-	 * @param variables
-	 */
+
 	private static void setVariables(String variables) {
 		jsonObject.put("variables", variables);
 	}
 	
-	/**
-	 * OPTIONAL<br>
-	 * set the request fragments if there are any, the request can be sent without fragments 
-	 * @param fragments
-	 */
+
 	private static void setFragments(String fragments) {
 		jsonObject.put("fragments", fragments);
 	}
 
-	/**
-	 * MANDATORY<br>
-	 * get the request body as a string value.
-	 * @return Request Body
-	 */
+
 	private static String getRequestBody() {
 
 		return jsonObject.toString();
 	}
-
+	
 	/**
-	 * MANDATORY <br>
-	 * Send Graphql Request Using The Request Body.<br>
-	 * Use this method after using "getRequestBody" method
-	 * @param body
+	 * Send Graphql Request Using "Query or Mutation" WITH Variables or Fragments.<br>
+	 * At least you should provide "Query or Mutation"
+	 * @param query -> MANDATORY
+	 * @param variables -> OPTIONAL (Nullable)
+	 * @param fragments -> OPTIONAL (Nullable)
 	 * @return Graphql Response
 	 */
-	public static Response sendGraphqlRequest(String query) {
-		
-		UtilityClass.setQuery(query);
-
-		System.out.println("sending graphql request...");
-		request.header("Content-Type", "application/json");
-		request.body(UtilityClass.getRequestBody());
-		request.given().log().body();
-		Response response = request.post(END_POINT);
-		System.out.println("getting graphql response...");
-		response.then().statusCode(200).log().body();
-		return response;
-		
-	}
-	
-	public static Response sendGraphqlRequest(String query, String variables) {
-		
-		UtilityClass.setQuery(query);
-		UtilityClass.setVariables(variables);
-
-		System.out.println("sending graphql request...");
-		request.header("Content-Type", "application/json");
-		request.body(UtilityClass.getRequestBody());
-		request.given().log().body();
-		Response response = request.post(END_POINT);
-		System.out.println("getting graphql response...");
-		response.then().statusCode(200).log().body();
-		return response;
-		
-	}
-	
 	public static Response sendGraphqlRequest(String query, String variables, String fragments) {
 		
-		UtilityClass.setQuery(query);
-		UtilityClass.setVariables(variables);
-		UtilityClass.setFragments(fragments);
-
-		System.out.println("sending graphql request...");
+		if(query != null) {
+		UtilityClass.setQuery(query);}
+		
+		if (variables !=null) {
+		UtilityClass.setVariables(variables);}
+		
+		if (fragments != null) {
+		UtilityClass.setFragments(fragments);}
+		
+		String requestBody = UtilityClass.getRequestBody();
+		
+		//request actions
+		System.out.println(">>> sending graphql request >>>");
 		request.header("Content-Type", "application/json");
-		request.body(UtilityClass.getRequestBody());
-		request.given().log().body();
-		Response response = request.post(END_POINT);
-		System.out.println("getting graphql response...");
+		request.body(requestBody);
+		request.given().log().all();
+		
+		//response actions
+		Response response = request.post(BASE_URI);
+		System.out.println("<<< getting graphql response <<<");
 		response.then().statusCode(200).log().body();
 		return response;
 		
